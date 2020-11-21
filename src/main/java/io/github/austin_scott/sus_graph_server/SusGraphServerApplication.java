ca398151726121e25d39854f693d914d7631332e
Austin Scott <12504656+Austin-Scott@users.noreply.github.com>
2020-11-21 14:32:16 -0600

    First commit

---
+++ src/main/java/io/github/austin_scott/sus_graph_server/SusGraphServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.austin_scott.sus_graph_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SusGraphServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SusGraphServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/austin_scott/sus_graph_server/SusGraphServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.austin_scott.sus_graph_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SusGraphServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
